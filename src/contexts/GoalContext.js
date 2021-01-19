7529b4baeefa16503f833a4ed9b6ad5493d671e8
Charan Gautam <gautamsaicharan@yahoo.com>
2021-01-19 16:17:59 -0700

    local storage

--- src/contexts/GoalContext.js
+++ src/contexts/GoalContext.js
@@ -1,10 +1,18 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useEffect } from 'react';
 import { goalReducer } from '../reducers/goalReducer'
 
 export const GoalContext = createContext();
 
 const GoalContextProvider = (props) => {
-    const [ goals, dispatch ] = useReducer(goalReducer, [])
+    const [ goals, dispatch ] = useReducer(goalReducer, [], () => {
+        const localData = localStorage.getItem('goals');
+        return localData ? JSON.parse(localData) : [];
+    })
+
+
+    useEffect(() => {
+        localStorage.setItem('goals', JSON.stringify(goals))
+    }, [goals])
 
     return(
         <GoalContext.Provider value={{ goals, dispatch}}>
