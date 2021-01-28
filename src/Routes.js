9308fc70ae19e1caf93ebc037a7b9bec1653c3fb
Charan Gautam <gautamsaicharan@yahoo.com>
2021-01-27 22:45:28 -0700

    start of home pg | css fixes

--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import GoalCentre from './components/goalCentre/GoalCentre';
 import FocusCentre from './components/focusCentre/FocusCentre';
+import Home from './components/home/Home';
 
 
 function Routes() { 
     return(
         <BrowserRouter>
             <Switch>
+                <Route exact path='/' component={Home} />
                 <Route exact path='/goalCentre' component={GoalCentre}/>
                 <Route exact path='/focusCentre' component={FocusCentre}/> 
             </Switch>
---
+++ src/components/home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './css/Home.css';
+
+const Home = () => {
+    return(
+        <div className='home'>
+            <h1>Welcome to Insula</h1>
+            <p>The mission of this application is to provide you with a variety of tools that works to manage, construct and ultilize your time to your liking. Time is valuable, get to it!</p>
+        </div>
+    )
+}
+
+export default Home;
