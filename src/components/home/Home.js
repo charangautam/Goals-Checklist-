3df4f654c9b63fd50eb22c6e40aa885999b22fa8
Charan Gautam <gautamsaicharan@yahoo.com>
2021-01-29 22:37:00 -0700

    home page css

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -11,8 +11,17 @@ const Home = () => {
                 <p>The mission of this application is to provide you with a variety of tools that works to manage, construct and ultilize your time to your liking. Time is valuable, get to it!</p>
             </div>
             <h2>Here are some of the tools available today</h2>
-            <GoalCentreLink />
-            <FocusCentreLink />
+            <div className='flex-container'>
+                <div className='flex-child first'>
+                    <GoalCentreLink />
+                </div>
+                <div className='flex-child second'>
+                    <FocusCentreLink />
+                </div>
+
+            </div>
+           
+            
         </div>
     )
 }
